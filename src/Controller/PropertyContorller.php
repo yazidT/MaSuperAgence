4020439144622d31ee77b54ebf7394efc685aeaa
yazidT <yazzidterfas@gmail.com>
2021-12-31 21:01:08 +0100

    Set a Property on the DB

--- src/Controller/PropertyContorller.php
+++ src/Controller/PropertyContorller.php
@@ -1,23 +1,44 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyContorller extends AbstractController
 {
-
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index (): Response
     {
+        /**
+         * Set a property in the database
+         
+         * 
+         */
+        $property = new Property();
+        $property->setTitle('Mon Premier Bien')
+                 ->setPrice(200000)
+                 ->setRooms(4)
+                 ->setBedrooms(3)
+                 ->setFloor(2)
+                 ->setSurface(60)
+                 ->setHeat(1)
+                 ->setCity('Montpellier')
+                 ->setAddress("12 cours Gambetta")
+                 ->setZipCode('34000');
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($property);
+        $manager->flush();
+
         return $this->render('property/index.html.twig',
                                 [
                                     'current_menu' => 'properties'
                                 ]);
     }
      
-}
+} 
