7028734371f5ffc103311342461aa74bc93ade34
yazidT <yazzidterfas@gmail.com>
2022-01-04 18:32:28 +0100

    translate forms labels en-fr

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -12,18 +12,18 @@ class PropertyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title',null,['label'=>'Titre :'])
-            ->add('description',null,['label'=>'Description :'])
-            ->add('surface',null,['label'=>'Surface :'])
-            ->add('rooms',null,['label'=>'Nombre de pièces :'])
-            ->add('bedrooms',null,['label'=>'Nombre de chambres :'])
-            ->add('floor',null,['label'=>'Etage :'])
-            ->add('price',null,['label'=>'Prix :'])
-            ->add('heat',null,['label'=>'Chauffage :'])
-            ->add('city',null,['label'=>'Ville :'])
-            ->add('address',null,['label'=>'Adresse :'])
-            ->add('zip_code',null,['label'=>'Code postale :'])
-            ->add('sold',null,['label'=>'Vendu :'])
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat')
+            ->add('city')
+            ->add('address')
+            ->add('zip_code')
+            ->add('sold')
         ;
     }
 
@@ -31,6 +31,7 @@ class PropertyType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Property::class,
+            'translation_domain' => 'forms'
         ]);
     }
 }
